93d5c8eb64435921b4fe9c8b8caaebf7e387f1a9
r-kozak <roman.kozak2085@gmail.com>
2023-01-19 20:17:15 +0200

    add first android files

---
+++ app/src/main/java/com/kozak/pw/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kozak.pw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kozak.pw.R.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
