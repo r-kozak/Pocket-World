44b6cccc76f5b1798ee21de60c92c2474d984fa4
r-kozak <roman.kozak2085@gmail.com>
2023-01-22 23:25:18 +0200

    replace person activity with fragment

--- app/src/main/java/com/kozak/pw/presentation/activity/PersonActivity.kt
+++ app/src/main/java/com/kozak/pw/presentation/activity/PersonActivity.kt
@@ -3,20 +3,14 @@ package com.kozak.pw.presentation.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.kozak.pw.PwConstants.DEFAULT_ITEM_ID
-import com.kozak.pw.R
 import com.kozak.pw.databinding.ActivityPersonBinding
-import com.kozak.pw.presentation.view_model.PersonViewModel
-import com.kozak.pw.short
+import com.kozak.pw.presentation.fragment.PersonFragment
 
 class PersonActivity : AppCompatActivity() {
+    private var currentPersonId: Long = DEFAULT_ITEM_ID
     private lateinit var binding: ActivityPersonBinding
-    private lateinit var viewModel: PersonViewModel
-    private var currentPersonId: Long? = null
 
     companion object {
         private const val INTENT_PERSON_ID = "intent_person_id"
@@ -30,98 +24,18 @@ class PersonActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        parseIntent()
-
         binding = ActivityPersonBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        viewModel = ViewModelProvider(this)[PersonViewModel::class.java]
-        setClickListeners()
-        addTextChangeListeners()
-        displayPersonData()
-        observeViewModel()
-    }
-
-    private fun displayPersonData() {
-        currentPersonId?.let { id ->
-            viewModel.getPersonItem(id)
-            viewModel.personItem.observe(this) {
-                binding.etFirstName.setText(it.firstName)
-                binding.etLastName.setText(it.lastName)
-                binding.etStrength.setText(it.strength.toString())
-                binding.tvBirthDate.text = it.birthDate.short()
-                binding.tvSex.text = it.sex.toString()
-                binding.tvDeathDate.text = it.deathDate.short()
-                binding.tvIsAlive.text = it.isAlive.toString()
-                binding.tvIsFavorite.text = it.isFavorite.toString()
-                binding.tvId.text = it.id.toString()
-            }
-        } ?: throw RuntimeException("Person ID is not defined!")
-    }
-
-    private fun addTextChangeListeners() {
-        binding.etFirstName.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                viewModel.resetErrorInputFirstName()
-            }
-            override fun afterTextChanged(s: Editable?) {}
-        })
-        binding.etLastName.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                viewModel.resetErrorInputLastName()
-            }
-            override fun afterTextChanged(s: Editable?) {}
-        })
-        binding.etStrength.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                viewModel.resetErrorInputStrength()
-            }
-            override fun afterTextChanged(s: Editable?) {}
-        })
+        parseIntent()
+        supportFragmentManager.beginTransaction()
+            .add(binding.personContainer.id, PersonFragment.newInstance(currentPersonId))
+            .commit()
     }
 
     private fun parseIntent() {
         currentPersonId = intent.getLongExtra(INTENT_PERSON_ID, DEFAULT_ITEM_ID)
-            .let { if (it == DEFAULT_ITEM_ID) null else it }
-    }
-
-    private fun setClickListeners() {
-        // Kill button
-        binding.killButton.setOnClickListener {
-            currentPersonId?.let {
-                viewModel.killPerson(it)
-            } ?: throw RuntimeException("Person ID is not defined!")
-        }
-        // Save button
-        binding.saveButton.setOnClickListener {
-            currentPersonId?.let { personId ->
-                viewModel.editPersonItem(
-                    personId,
-                    binding.etFirstName.editableText.toString(),
-                    binding.etLastName.editableText.toString(),
-                    binding.etStrength.editableText.toString()
-                )
-            } ?: throw RuntimeException("Person ID is not defined!")
-        }
-    }
-
-    private fun observeViewModel() {
-        viewModel.errorInputFirstName.observe(this) { isError ->
-            binding.tilFirstName.error = if (isError) getString(R.string.error_input_first_name)
-            else null
-        }
-        viewModel.errorInputLastName.observe(this) { isError ->
-            binding.tilLastName.error = if (isError) getString(R.string.error_input_last_name)
-            else null
-        }
-        viewModel.errorInputStrength.observe(this) { isError ->
-            binding.tilStrength.error = if (isError) getString(R.string.error_input_strength)
-            else null
-        }
-        viewModel.shouldCloseScreen.observe(this) {
-            finish()
+        if (currentPersonId == DEFAULT_ITEM_ID) {
+            throw RuntimeException("Param person id is absent")
         }
     }
 }
---
+++ app/src/main/java/com/kozak/pw/presentation/fragment/PersonFragment.kt
@@ -0,0 +1,142 @@
+package com.kozak.pw.presentation.fragment
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.kozak.pw.PwConstants
+import com.kozak.pw.R
+import com.kozak.pw.databinding.FragmentPersonBinding
+import com.kozak.pw.presentation.view_model.PersonViewModel
+import com.kozak.pw.short
+
+class PersonFragment : Fragment() {
+
+    private lateinit var viewModel: PersonViewModel
+    private lateinit var binding: FragmentPersonBinding
+    private var currentPersonId: Long = PwConstants.DEFAULT_ITEM_ID
+
+    companion object {
+        private const val PERSON_ID = "person_id"
+
+        fun newInstance(personId: Long): PersonFragment {
+            return PersonFragment().apply {
+                arguments = Bundle().apply {
+                    putLong(PERSON_ID, personId)
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseParams()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+         binding = FragmentPersonBinding.inflate(inflater, container, false)
+         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[PersonViewModel::class.java]
+        setClickListeners()
+        addTextChangeListeners()
+        displayPersonData()
+        observeViewModel()
+    }
+
+    private fun parseParams() {
+        val args = requireArguments()
+        if (!args.containsKey(PERSON_ID)) {
+            throw RuntimeException("Param person id is absent")
+        }
+        currentPersonId = args.getLong(PERSON_ID, PwConstants.DEFAULT_ITEM_ID)
+    }
+
+    private fun setClickListeners() {
+        // Kill button
+        binding.killButton.setOnClickListener {
+            viewModel.killPerson(currentPersonId)
+        }
+        // Save button
+        binding.saveButton.setOnClickListener {
+            viewModel.editPersonItem(
+                currentPersonId,
+                binding.etFirstName.editableText.toString(),
+                binding.etLastName.editableText.toString(),
+                binding.etStrength.editableText.toString()
+            )
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.errorInputFirstName.observe(viewLifecycleOwner) { isError ->
+            binding.tilFirstName.error = if (isError) getString(R.string.error_input_first_name)
+            else null
+        }
+        viewModel.errorInputLastName.observe(viewLifecycleOwner) { isError ->
+            binding.tilLastName.error = if (isError) getString(R.string.error_input_last_name)
+            else null
+        }
+        viewModel.errorInputStrength.observe(viewLifecycleOwner) { isError ->
+            binding.tilStrength.error = if (isError) getString(R.string.error_input_strength)
+            else null
+        }
+        viewModel.shouldCloseScreen.observe(viewLifecycleOwner) {
+            activity?.onBackPressedDispatcher?.onBackPressed()
+        }
+    }
+
+
+    private fun displayPersonData() {
+        viewModel.getPersonItem(currentPersonId)
+        viewModel.personItem.observe(viewLifecycleOwner) {
+            binding.etFirstName.setText(it.firstName)
+            binding.etLastName.setText(it.lastName)
+            binding.etStrength.setText(it.strength.toString())
+            binding.tvBirthDate.text = it.birthDate.short()
+            binding.tvSex.text = it.sex.toString()
+            binding.tvDeathDate.text = it.deathDate.short()
+            binding.tvIsAlive.text = it.isAlive.toString()
+            binding.tvIsFavorite.text = it.isFavorite.toString()
+            binding.tvId.text = it.id.toString()
+        }
+    }
+
+    private fun addTextChangeListeners() {
+        binding.etFirstName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorInputFirstName()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        binding.etLastName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorInputLastName()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        binding.etStrength.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorInputStrength()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+
+}
