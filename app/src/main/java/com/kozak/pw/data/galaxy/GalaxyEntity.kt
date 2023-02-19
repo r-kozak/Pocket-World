1eade76edcbcceeee9844c9cbb363d28cc72313d
r-kozak <roman.kozak2085@gmail.com>
2023-02-19 22:24:22 +0200

    add ForeignKey of UniverseEntity to GalaxyEntity.kt

--- app/src/main/java/com/kozak/pw/data/galaxy/GalaxyEntity.kt
+++ app/src/main/java/com/kozak/pw/data/galaxy/GalaxyEntity.kt
@@ -1,11 +1,20 @@
 package com.kozak.pw.data.galaxy
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.Companion.CASCADE
 import androidx.room.PrimaryKey
 import com.kozak.pw.data.PwAnyEntity
 import com.kozak.pw.data.TablesNamesConstants
+import com.kozak.pw.data.universe.UniverseEntity
 
-@Entity(tableName = TablesNamesConstants.GALAXY_ENTITY_TABLE_NAME)
+@Entity(
+    tableName = TablesNamesConstants.GALAXY_ENTITY_TABLE_NAME,
+    foreignKeys = [ForeignKey(
+        entity = UniverseEntity::class, childColumns = arrayOf("universeId"),
+        parentColumns = arrayOf("id"), onDelete = CASCADE
+    )]
+)
 data class GalaxyEntity(
     @PrimaryKey(autoGenerate = true)
     override val id: Long,
@@ -13,6 +22,6 @@ data class GalaxyEntity(
     override var name: String,
     override var health: Int,
     override var mass: Long,
-    // TODO add ForeignKey param to Entity annotation to implement relations
+    // ForeignKey of Universe to set relation
     val universeId: Long
 ) : PwAnyEntity()
